cc6ce5dc8e1b39ced634998c0e5c1c1769a2825d
xuhaoyan <xuhaoyan@heyoutec.com>
2026-08-21 10:20:32 +0800

    - 添加carousel_slider插件 - 实现轮播图效果渲染

--- lib/component/Home/HmSlider.dart
+++ lib/component/Home/HmSlider.dart
@@ -1,20 +1,47 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HmSlider extends StatefulWidget {
-  HmSlider({Key? key}) : super(key: key);
+  List<BannerItem> bannerList;
+  HmSlider({super.key, required this.bannerList});
 
   @override
-  _HmSliderState createState() => _HmSliderState();
+  State<StatefulWidget> createState() => _HmSliderState();
 }
 
 class _HmSliderState extends State<HmSlider> {
+  Widget _getSlider() {
+    // 在Flutter中获取屏幕宽度的方法
+    final double screenWidth = MediaQuery.of(context).size.width; // 屏幕宽度
+    // 返回轮播图插件
+    // 根据数据渲染不同的轮播选项
+    return CarouselSlider(
+      items: List.generate(widget.bannerList.length, (index) {
+        return Image.network(
+          widget.bannerList[index].imgUrl ?? "",
+          fit: BoxFit.cover,
+          width: screenWidth,
+        );
+      }),
+      options: CarouselOptions(
+        height: 300,
+        autoPlay: true, // 自动播放
+        viewportFraction: 1, // 视窗比例
+        autoPlayInterval: Duration(seconds: 5), // 自动播放时间
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      color: Colors.blue,
-      alignment: Alignment.center,
-      child: Text("轮播图", style: TextStyle(color: Colors.white, fontSize: 20)),
-    );
+    // stack -> 轮播图 搜索框 指示灯导航
+    return Stack(children: [_getSlider()]);
+    // return Container(
+    //   height: 300,
+    //   color: Colors.blue,
+    //   alignment: Alignment.center,
+    //   child: Text("轮播图", style: TextStyle(color: Colors.white, fontSize: 20)),
+    // );
   }
 }
--- lib/pages/Home/index.dart
+++ lib/pages/Home/index.dart
@@ -5,6 +5,7 @@ import 'package:hm_shop/component/Home/HmHot.dart';
 import 'package:hm_shop/component/Home/HmMoreList.dart';
 import 'package:hm_shop/component/Home/HmSlider.dart';
 import 'package:hm_shop/component/Home/HmSuggestion.dart';
+import 'package:hm_shop/viewmodels/home.dart';
 
 class HomeView extends StatefulWidget {
   HomeView({Key? key}) : super(key: key);
@@ -14,11 +15,27 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
+  // 注释: 轮播图数据
+  final List<BannerItem> _bannerItem = [
+    BannerItem(
+      id: "1",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/1.jpg",
+    ),
+    BannerItem(
+      id: "2",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/2.png",
+    ),
+    BannerItem(
+      id: "3",
+      imgUrl: "https://yjy-teach-oss.oss-cn-beijing.aliyuncs.com/meituan/3.jpg",
+    ),
+  ];
+
   // 注释: 获取滚动容器的内容
   List<Widget> _getScrollChildern() {
     return [
       // 包裹普通widget的sliver家族的组件
-      SliverToBoxAdapter(child: HmSlider()), // 轮播图组件
+      SliverToBoxAdapter(child: HmSlider(bannerList: _bannerItem)), // 轮播图组件
       // 放置分类组件
       SliverToBoxAdapter(child: SizedBox(height: 10)),
 
